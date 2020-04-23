bc3c1ae9c3e379e4b4c00afd414d0dec3fc4676a
patriciachedid <patriciachedid@gmail.com>
2020-04-23 19:16:31 -0300

    adding yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Restaurant.destroy_all
+
+puts "database is clean"
+
+puts 'Creating restaurants'
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w(chinese italian japanese french belgian).sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+
+  puts "restaurant #{restaurant.id} is created!"
+end
